7ef1ccdb40d6cbde131b181b6e54e3b5e4b32125
Tobias Jansson <tobias.jansson@gmail.com>
2019-10-22 07:32:03 +0200

    Comment in javascript broke the script when on one line

--- htmlStrings.sh
+++ htmlStrings.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cat accesspoint.html | tr -d '\n' | sed 's/\"/\\\"/g' > accesspoint-oneline.txt
-cat main.html | tr -d '\n' | sed 's/\"/\\\"/g' > main-online.txt
+cat main.html | tr -d '\n' | sed 's/\"/\\\"/g' > main-oneline.txt
